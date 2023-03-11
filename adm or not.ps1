cbcf14374f01f3bf3296e72b44657c5bccc25c46
VVolfCZ <83589607+VVolfCZ@users.noreply.github.com>
2023-03-11 22:27:42 +0100

    Delete adm or not.ps1

--- adm or not.ps1
+++
@@ -1,7 +0,0 @@
-$admin = [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")
-
-if ($admin) {
-    Write-Host "Jsi administrátor."
-} else {
-    Write-Host "Nejsi administrátor."
-}
