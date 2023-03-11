bc1d0fc9fa6f64d29f2714bf6422ade3d809514d
VVolfCZ <83589607+VVolfCZ@users.noreply.github.com>
2023-03-11 09:18:32 +0100

    Update 77.ps1

--- 77.ps1
+++ 77.ps1
@@ -1,8 +1,20 @@
-$admin = [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")
+# Zjisti, zda je uživatel admin
+$currentUser = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+$isAdmin = $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
 
-if ($admin) {
-    Set-Content -Path "$home\Desktop\tuodpoved.txt" -Value "admin jsi"
-
-} else {
+# Pokud uživatel není admin, spusť kód pro běžného uživatele
+if (-not $isAdmin) {
+    Write-Output "Jsi běžný uživatel."
+    # Sem napiš kód pro běžného uživatele
     Set-Content -Path "$home\Desktop\tuodpoved.txt" -Value "admin nejsi"
+}
+else {
+    Write-Output "Jsi administrátor."
+    # Pokud jsi admin, spusť skript znovu jako admin
+    $args = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+    Start-Process powershell.exe -Verb RunAs -ArgumentList $args
+    # Ukonči aktuální instanci skriptu
+    Exit
+    # Sem napiš kód pro administrátora
+    Set-Content -Path "$home\Desktop\tuodpoved.txt" -Value "admin jsi"
 }
